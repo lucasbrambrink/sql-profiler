90fde951694d93362c065481e23f27d132bf05c0
Lucas Brambrink <lucasbrambrink@Lucass-MacBook-Pro.local>
2019-11-16 04:17:47 -0500

    adds much simpler sql profiler

---
+++ src/sql_profiler.py
@@ -0,0 +1,54 @@
+from hashlib import sha1
+from django.db import connection
+
+
+class ParseSql:
+
+    def __init__(self, sql_statements):
+        self.sql_statements = sql_statements
+        self.sql_statements.sort(key=lambda x: float(x['time']))
+        self.analyzed = [self.analyze_query(q) for q in self.sql_statements]
+        self.sql = self._deduplicate_sql_expressions()
+        self.top_ten = self.sql[-10:]
+
+    @staticmethod
+    def analyze_query(query):
+        sql = query['sql']
+        num_joins = sql.count('JOIN')
+        return {
+            'num_joins': num_joins,
+            'time': float(query['time']),
+            'query': sql,
+            'hash': sha1(sql.encode()).hexdigest(),
+        }
+
+    def _deduplicate_sql_expressions(self):
+        results = defaultdict(int)
+        statements = {}
+        for s in self.analyzed:
+            results[s['hash']] += 1
+            statements[s['hash']] = s
+
+        profile = []
+        for hash_id, count in results.items():
+            statement = statements[hash_id]
+            profile.append({
+                'count': count,
+                'individual_time': statement['time'],
+                'total_time': count * statement['time'],
+                'statement': statement,
+            })
+        profile.sort(key=lambda r: r['count'])
+        return profile
+
+
+def profile_sql(func, *args, **kwargs):
+    """
+    Executes `func` and returns SQL analysis
+    """
+    starting_connections_num = len(connection.queries)
+
+    func(*args, **kwargs)
+
+    ending_connections = list(connection.queries)
+    return ParseSql(ending_connections[starting_connections_num:])
